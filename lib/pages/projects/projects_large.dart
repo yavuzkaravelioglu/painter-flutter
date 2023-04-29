a5a1937d0f2feca7d443a99c3266855eaf309a36
yavuz karavelioglu <>
2023-04-29 23:36:51 +0300

    projects page large started, imageslideshow with full screen image view implemented

--- lib/pages/projects/projects_large.dart
+++ lib/pages/projects/projects_large.dart
@@ -3,6 +3,9 @@
 import 'package:flutter/material.dart';
 
 import '../../widgets/text_widgets/font_advent_text.dart';
+import '../../widgets/text_widgets/font_orbitron_text.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:easy_image_viewer/easy_image_viewer.dart';
 
 class ProjectsLarge extends StatelessWidget {
   const ProjectsLarge({super.key});
@@ -26,8 +29,161 @@ class ProjectsLarge extends StatelessWidget {
               ),
             ),
           ),
+          Padding(
+            padding: EdgeInsetsDirectional.symmetric(
+              vertical: screenSize.height * 0.05,
+              horizontal: screenSize.width * 0.2,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FontOrbitronText(
+                  text: "The projects that\nwe proud of",
+                  fontSize: 43,
+                  color: Colors.black,
+                  letterSpacing: 2,
+                  containerAlign: Alignment.topLeft,
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(
+                  height: screenSize.height * 0.08,
+                ),
+                Icon(
+                  Icons.keyboard_arrow_down_rounded,
+                  color: Colors.black,
+                  size: 42.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                SizedBox(
+                  height: screenSize.height * 0.08,
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      ImageSlideShowWithTap(screenSize: screenSize),
+                      /* SizedBox(
+                        height: 200,
+                      ),
+                      ImageSlideShow(screenSize: screenSize), */
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
+    );
+  }
+}
+
+class ImageSlideShowWithTap extends StatefulWidget {
+  const ImageSlideShowWithTap({
+    super.key,
+    required this.screenSize,
+  });
+
+  final Size screenSize;
+
+  @override
+  State<ImageSlideShowWithTap> createState() => _ImageSlideShowWithTapState();
+}
+
+class _ImageSlideShowWithTapState extends State<ImageSlideShowWithTap> {
+  final List<ImageProvider> _imageProviders = [
+    Image.asset(
+      'assets/photos/a-1.jpeg',
+      fit: BoxFit.cover,
+    ).image,
+  ];
+
+  late final _easyEmbeddedImageProvider = MultiImageProvider(_imageProviders);
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageSlideshow(
+      height: widget.screenSize.height * 0.5,
+      indicatorColor: const Color.fromARGB(255, 114, 176, 227),
+      indicatorBackgroundColor: Colors.white,
+      autoPlayInterval: 6000,
+      isLoop: true,
+      children: [
+        GestureDetector(
+          child: Hero(
+            tag: "a-1",
+            child: Image.asset(
+              'assets/photos/a-1.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          onTap: () {
+            showImageViewer(
+                context, Image.asset('assets/photos/a-1.jpeg').image,
+                swipeDismissible: true, doubleTapZoomable: true);
+          },
+        ),
+        GestureDetector(
+          child: Hero(
+            tag: "b-1",
+            child: Image.asset(
+              'assets/photos/b-1.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          onTap: () {
+            showImageViewer(
+                context, Image.asset('assets/photos/b-1.jpeg').image,
+                swipeDismissible: true, doubleTapZoomable: true);
+          },
+        ),
+        GestureDetector(
+          child: Hero(
+            tag: "c-1",
+            child: Image.asset(
+              'assets/photos/c-1.jpeg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          onTap: () {
+            showImageViewer(
+                context, Image.asset('assets/photos/c-1.jpeg').image,
+                swipeDismissible: true, doubleTapZoomable: true);
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ImageSlideShow extends StatelessWidget {
+  const ImageSlideShow({
+    super.key,
+    required this.screenSize,
+  });
+  final Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageSlideshow(
+      height: screenSize.height * 0.5,
+      indicatorColor: const Color.fromARGB(255, 114, 176, 227),
+      indicatorBackgroundColor: Colors.white,
+      autoPlayInterval: 5000,
+      isLoop: true,
+      children: [
+        Image.asset(
+          'assets/photos/a-1.jpeg',
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'assets/photos/b-1.jpeg',
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          'assets/photos/c-1.jpeg',
+          fit: BoxFit.cover,
+        ),
+      ],
     );
   }
 }
--- lib/pages/projects/projects_small.dart
+++ lib/pages/projects/projects_small.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:paint_burak/widgets/text_widgets/font_advent_text.dart';
 
+import '../../widgets/text_widgets/font_orbitron_text.dart';
+
 class ProjectsSmall extends StatelessWidget {
   const ProjectsSmall({super.key});
 
@@ -22,6 +24,37 @@ class ProjectsSmall extends StatelessWidget {
                 opacity: 0.4,
                 fit: BoxFit.cover,
               ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.symmetric(
+              vertical: screenSize.height * 0.05,
+              horizontal: screenSize.width * 0.1,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FontOrbitronText(
+                  text: "The projects that\nwe proud of",
+                  fontSize: 23,
+                  color: Colors.black,
+                  letterSpacing: 2,
+                  containerAlign: Alignment.topLeft,
+                  textAlign: TextAlign.left,
+                ),
+                SizedBox(
+                  height: screenSize.height * 0.06,
+                ),
+                Icon(
+                  Icons.keyboard_arrow_down_rounded,
+                  color: Colors.black,
+                  size: 22.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                SizedBox(
+                  height: screenSize.height * 0.06,
+                ),
+              ],
             ),
           ),
         ],
